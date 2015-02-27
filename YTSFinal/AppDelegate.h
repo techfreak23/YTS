27cdc01a3d19fc17470d69db2e829b2c7f29dea9
techfreak23 <mac@techfreaks-MacBook-Pro.local>
2015-02-26 19:21:51 -0800

    Initial Commit

---
+++ YTSFinal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTSFinal
+//
+//  Created by Art Sevilla on 2/26/15.
+//  Copyright (c) 2015 Art Sevilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YTSFinal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTSFinal
+//
+//  Created by Art Sevilla on 2/26/15.
+//  Copyright (c) 2015 Art Sevilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
